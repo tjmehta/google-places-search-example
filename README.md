c3566ae53c05edc0842f9101c928a32b4c1f1d51
tjmehta <tejesh.mehta@gmail.com>
2016-05-25 18:26:19 -0700

    initial example

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # google-places-search-example
-google places search example in javascript
+google places search script example in node.js
+
+# Usage
+Modify Input (on index.js line 8):
+```js
+var input = [
+  {
+    name: 'philz coffee',
+    address: '399 Golden Gate Ave, San Francisco, CA 94102'
+  }
+]
+// ...
+```
+
+Command:
+```bash
+GOOGLE_API_KEY=<google_api_key> node index.js
+```
+
+Output:
+```
+{
+  "name": "philz coffee",
+  "address": "399 Golden Gate Ave, San Francisco, CA 94102",
+  "google": {
+    "id": "ChIJH7w1a5qAhYARSHYy_wjbPr4",
+    "name": "Philz Coffee",
+    "address": "399 Golden Gate Ave, San Francisco, CA 94102, United States",
+    "phoneNumber": "(415) 621-7000"
+  },
+}
+```
+
+License
+MIT
---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict'
+
+var assign = require('101/assign')
+var map = require('async').map
+var googlePlaces = require('./lib/google-places-client.js')
+
+// input data
+var input = [
+  {
+    name: 'philz coffee',
+    address: '399 Golden Gate Ave, San Francisco, CA 94102'
+  }
+]
+
+// get data from google
+var _i = 0
+map(input, function (inItem, cb) {
+  googlePlaces.getByNameAndAddress(inItem, function (err, json) {
+    var outItem = assign({}, inItem)
+
+    if (err) {
+      outItem.err = err
+      cb(null, outItem)
+      return
+    }
+
+    var firstResult = json.results[0]
+
+    if (!firstResult) {
+      outItem.err = new Error('no places found :(')
+      cb(null, outItem)
+      return
+    }
+
+    outItem.google = {
+      id: firstResult.place_id,
+      name: firstResult.name,
+      address: firstResult.formatted_address
+    }
+
+    if (json.results.length > 1) {
+      console.warn(inItem.name + ' resulted in multiple results..')
+      console.warn(JSON.stringify(json.results, null, 2))
+    }
+
+    googlePlaces.getDetailsById(outItem.google.id, function (err, details) {
+      if (err) {
+        outItem.err = err
+        cb(null, outItem)
+        return
+      }
+      outItem.google.phoneNumber = details.result.formatted_phone_number
+      cb(null, outItem)
+    })
+  })
+
+}, done)
+
+function done (err, output) {
+  // print
+  output.forEach(function (item) {
+    console.log(JSON.stringify(item, null, 2))
+  })
+}
